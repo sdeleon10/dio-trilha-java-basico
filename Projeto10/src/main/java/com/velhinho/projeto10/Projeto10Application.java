c07699faad87b73d364aadc08c69e4783bc11129
John DoeDeleon <sdeleon10@gmail.com>
2025-08-29 10:30:38 -0300

    projeto10

---
+++ Projeto10/src/main/java/com/velhinho/projeto10/Projeto10Application.java
@@ -0,0 +1,13 @@
+package com.velhinho.projeto10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto10Application.class, args);
+	}
+
+}
---
+++ Projeto10/src/test/java/com/velhinho/projeto10/Projeto10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velhinho.projeto10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
